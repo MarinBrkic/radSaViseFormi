cb3a01c8d9eb912384bfe1f67420a8a284a0dd62
MarinBrkic <152974525+MarinBrkic@users.noreply.github.com>
2024-10-10 10:01:02 +0200

    Add files via upload

---
+++ radSaViseFormi/radSaViseFormi/studenti.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace radSaViseFormi
+{
+    partial class studenti
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonDodaj = new System.Windows.Forms.Button();
+            this.buttonObrisiStudenta = new System.Windows.Forms.Button();
+            this.buttonObrisiSve = new System.Windows.Forms.Button();
+            this.listBoxStudenti = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // buttonDodaj
+            // 
+            this.buttonDodaj.Location = new System.Drawing.Point(16, 21);
+            this.buttonDodaj.Name = "buttonDodaj";
+            this.buttonDodaj.Size = new System.Drawing.Size(206, 33);
+            this.buttonDodaj.TabIndex = 0;
+            this.buttonDodaj.Text = "Dodaj novog studenta";
+            this.buttonDodaj.UseVisualStyleBackColor = true;
+            // 
+            // buttonObrisiStudenta
+            // 
+            this.buttonObrisiStudenta.Location = new System.Drawing.Point(366, 134);
+            this.buttonObrisiStudenta.Name = "buttonObrisiStudenta";
+            this.buttonObrisiStudenta.Size = new System.Drawing.Size(136, 38);
+            this.buttonObrisiStudenta.TabIndex = 1;
+            this.buttonObrisiStudenta.Text = "Obrisi studenta";
+            this.buttonObrisiStudenta.UseVisualStyleBackColor = true;
+            // 
+            // buttonObrisiSve
+            // 
+            this.buttonObrisiSve.Location = new System.Drawing.Point(16, 238);
+            this.buttonObrisiSve.Name = "buttonObrisiSve";
+            this.buttonObrisiSve.Size = new System.Drawing.Size(159, 32);
+            this.buttonObrisiSve.TabIndex = 2;
+            this.buttonObrisiSve.Text = "Obrisi sve studente";
+            this.buttonObrisiSve.UseVisualStyleBackColor = true;
+            this.buttonObrisiSve.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBoxStudenti
+            // 
+            this.listBoxStudenti.FormattingEnabled = true;
+            this.listBoxStudenti.ItemHeight = 20;
+            this.listBoxStudenti.Location = new System.Drawing.Point(16, 68);
+            this.listBoxStudenti.Name = "listBoxStudenti";
+            this.listBoxStudenti.Size = new System.Drawing.Size(344, 164);
+            this.listBoxStudenti.TabIndex = 3;
+            // 
+            // studenti
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 306);
+            this.Controls.Add(this.listBoxStudenti);
+            this.Controls.Add(this.buttonObrisiSve);
+            this.Controls.Add(this.buttonObrisiStudenta);
+            this.Controls.Add(this.buttonDodaj);
+            this.Name = "studenti";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonDodaj;
+        private System.Windows.Forms.Button buttonObrisiStudenta;
+        private System.Windows.Forms.Button buttonObrisiSve;
+        private System.Windows.Forms.ListBox listBoxStudenti;
+    }
+}
+
